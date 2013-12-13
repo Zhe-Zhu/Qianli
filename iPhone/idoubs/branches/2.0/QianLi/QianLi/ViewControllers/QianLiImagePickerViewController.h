0cc51f1441ff825da81d73a9415ee8b6997e9c73
LG <lt2010cuhk@gmail.com>
2013-12-13 18:32:33 +0800

    hide status bar when take photo using uiimagepicker

---
+++ iPhone/idoubs/branches/2.0/QianLi/QianLi/ViewControllers/QianLiImagePickerViewController.h
@@ -0,0 +1,13 @@
+//
+//  QianLiImagePickerViewController.h
+//  QianLi
+//
+//  Created by LG on 12/13/13.
+//  Copyright (c) 2013 Ash Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QianLiImagePickerViewController : UIImagePickerController
+
+@end
