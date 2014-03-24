5ce262f1860f157583e39e4d0b4a2e3129cc1e0d
LG <lt2010cuhk@gmail.com>
2014-03-24 09:36:54 +0800

    modification

--- iPhone/idoubs/branches/2.0/QianLi/QianLi/Global/Global.h
+++ iPhone/idoubs/branches/2.0/QianLi/QianLi/Global/Global.h
@@ -66,9 +66,9 @@ nil]
 #define kImageBaloonIn @"baloon_in"
 #define kImageBaloonOut @"baloon_out"
 
-//115.28.209.97
-#define kBaseURL @"http://112.124.36.134:8080"
-#define kServerIP @"112.124.36.134"
+//115.28.209.97 112.124.36.134
+#define kBaseURL @"http://115.28.209.97:8080"
+#define kServerIP @"115.28.209.97"
 #define kSemiModalAnimationDuration 0.3f
 
 #define kInCall @"UserInCall"
