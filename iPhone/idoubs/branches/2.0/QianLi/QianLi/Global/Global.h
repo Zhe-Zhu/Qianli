fad83e3f13368cb0c32391554ef85b280e267532
LG <lt2010cuhk@gmail.com>
2014-03-29 16:52:25 +0800

    change to production server ip

--- iPhone/idoubs/branches/2.0/QianLi/QianLi/Global/Global.h
+++ iPhone/idoubs/branches/2.0/QianLi/QianLi/Global/Global.h
@@ -73,9 +73,9 @@ nil]
 #define kSemiModalAnimationDuration 0.3f
 
 //115.28.209.97  //112.124.36.134 www.qlcall.com
-//#define kBaseURL [NSString stringWithFormat:@"http://%@:8080", [[NSUserDefaults standardUserDefaults] objectForKey:kHostIPKey]]
-#define kBaseURL [NSString stringWithFormat:@"http://%@:8080", @"115.28.209.97"]
-#define kServerIP @"115.28.209.97"
+#define kBaseURL [NSString stringWithFormat:@"http://%@:8080", [[NSUserDefaults standardUserDefaults] objectForKey:kHostIPKey]]
+//#define kBaseURL [NSString stringWithFormat:@"http://%@:8080", @"115.28.209.97"]
+#define kServerIP @"112.124.36.134"
 #define kHostIPKey @"HostIP"
 
 #define kSemiModalAnimationDuration 0.3f
